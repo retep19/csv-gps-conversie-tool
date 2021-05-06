ea50021e193e5748fa1126ad2a66a10df2540d7f
retep19 <retep_19@hotmail.com>
2021-05-06 10:37:49 +0200

    release v1.1. - updated fixed hanging on unknonw csv's. rewritten checking csv to common format. updated user interface ( more response). fixed no update gps on coordinates outside NL ( to OFN HQ coordinates)

--- csv-gps-conversie-tool.cpp
+++ csv-gps-conversie-tool.cpp
@@ -13,8 +13,8 @@
 
 using namespace csv;
 
-//verwachtte opmaak csv bestand
-static std::vector<std::string> col_cmp = {"Naam","Paneel","Face id","Object model","Paneel model","Object locatie","Locatie naam","Locatietype","Straat","Nummer","Plaats","Bushalte","Metro","Hectometer","Wegnummer","Opmerkingen","Wisselovereenkomst","Serviceovereenkomsten","GPS Latitude","GPS Longitude","N/A"};
+//verwachtte opmaak csv bessy objecten_import bestand
+static std::vector<std::string> colnames_obj_bessy_import = {"Naam","Paneel","Face id","Object model","Paneel model","Object locatie","Locatie naam","Locatietype","Straat","Nummer","Plaats","Bushalte","Metro","Hectometer","Wegnummer","Opmerkingen","Wisselovereenkomst","Serviceovereenkomsten","GPS Latitude","GPS Longitude","N/A"};
 
 #define MAX_LOADSTRING 100
 #define PROGRESSBAR_TIMER_ID 1
@@ -310,7 +310,6 @@ void opencsvfile(HWND hWnd) {
 
                         std::wstring filepath_w = pszFilePath;
                         std::string filepath_s(filepath_w.begin(), filepath_w.end());
-                        std::tm reportdate;
 
                         //test if it is a csv file
                         if (filepath_s.substr(filepath_s.length() - 3) != "csv") {
@@ -320,36 +319,19 @@ void opencsvfile(HWND hWnd) {
                             return;
                         }
 
-                        int n_rows, n_cols;
-                        double delta_progress;
-                        std::vector<std::string> colnames;
-                        try {
-                            auto info = get_file_info(filepath_s);
-                            n_rows = info.n_rows;
-                            n_cols = info.n_cols;
-                            colnames = info.col_names;
-                            delta_progress = 100 / n_rows;
-                            messagestream.str(L"");
-                            messagestream << L"processing: " << pszFilePath << L"\n [" << n_rows << "]rows in file";
-                            RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_INTERNALPAINT);
-                        }
-                        catch (std::exception& e) {
-                            send_debug_msg(e.what());
-                            send_log_error(e.what());
-                            messagestream.str(L"");
-                            messagestream << L"bestand geopend in ander programma, of inhoud niet herkend. Bestand niet ingelezen. Klik op file->open om een importbestand in te lezen";
-                            RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_INTERNALPAINT);
-                            return;
-                        }
+                        //find max number of lines
+                        std::ifstream inFile(filepath_s);
+                        int n_rows = std::count(std::istreambuf_iterator<char>(inFile),
+                            std::istreambuf_iterator<char>(), '\n');
 
                         //make modeless progress dialog
                         if (!IsWindow(hwndDLGPB)) {
-                            hwndDLGPB = CreateDialogParam(hInst, MAKEINTRESOURCE(IDD_PROGRESS), hWnd, ProgressProc, n_rows + 1);
+                            hwndDLGPB = CreateDialogParam(hInst, MAKEINTRESOURCE(IDD_PROGRESS), hWnd, ProgressProc, n_rows);
                             ShowWindow(hwndDLGPB, SW_SHOW);
                         }
 
                         //read import file  as csv as new thread
-                        std::thread task(read_bessy_object_importfile, std::ref(hwndDLGPB), colnames, filepath_s);
+                        std::thread task(read_import_file, std::ref(hwndDLGPB), pszFilePath);
                         task.detach();
 
                     }
@@ -362,60 +344,164 @@ void opencsvfile(HWND hWnd) {
     }
 }
 
+//threaded task //open import file, read creation date , when found/opened verwerk importbestand.
+void read_import_file(HWND hWndDLG, PWSTR pszFilePath) {
 
+    int parsed;
+    std::stringstream creation_date;
+    std::wstring filepath_w = pszFilePath;
+    std::string filepath_s(filepath_w.begin(), filepath_w.end());
+    HANDLE hFile;
+    std::tm Peildatum;
 
-void read_bessy_object_importfile(HWND hWndDLG, std::vector<std::string> colnames, std::string filepath_s) {
+    //print out filelocation in log and listbox
+    send_log_lbox_info(hWndDLG, filepath_s);
 
-    std::stringstream log_str;
-    std::string log_path;
+    //zoek bestandscreatiedatum en zet als Peildatum
+    hFile = CreateFile(pszFilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
 
-    if (check_bessy_objectenimport(hWndDLG, colnames) == 1) {
-        //parse schade import file
-        if (parse_bessy_objectenimport(hWndDLG, filepath_s) < 1) {
-            return;
-        }
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        send_log_lbox_error(hWndDLG, "INVALID_HANDLE_VALUE hfile, bestand niet kunnen openen.");
+        CloseHandle(hFile);
+        goto end;
+    }
+    else if (!GETFileCreationDate(hFile, Peildatum)) {
+        send_log_lbox_error(hWndDLG, "Bestandsdatum niet kunnen lezen [Peildatum], bestand niet kunnen openen.");
+        CloseHandle(hFile);
+        goto end;
     }
     else {
-        messagestream.str(L"");
-        messagestream << L"Inhoud importbestand niet herkend, bestand niet ingelezen. Klik op file->open om een importbestand in te lezen";
-        return;
+        creation_date << "Peildatum gevonden [" << Peildatum.tm_mday << "-" << (Peildatum.tm_mon + 1) << "-" << (Peildatum.tm_year + 1900) << "]";
+        send_log_lbox_info(hWndDLG, creation_date.str());
+        CloseHandle(hFile);
     }
-    messagestream.str(L"");
-    messagestream << L"Importbestand ingelezen. Klik op file->open om een volgend importbestand in te lezen";
+
+    //verwerk importbestand
+    parse_import_file(hWndDLG, filepath_s, Peildatum);
+
+end:
+    //maak button "Ok" beschikbaar om door te gaan. (direct without logging)
+    SendDlgItemMessage(hWndDLG, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
+    SendDlgItemMessage(hWndDLG, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L"Klik op ok om door te gaan");
+    SendDlgItemMessage(hWndDLG, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
+    EnableWindow(GetDlgItem(hWndDLG, IDOK), TRUE);
+    return;
+
+
+
 }
 
-int check_bessy_objectenimport(HWND hWnd, std::vector<std::string> col_names) {
-    
-    HWND hwndProgressBar = GetDlgItem(hWnd, IDC_PROGRESS1);
-    
-    if (col_cmp.size() != col_names.size() + 1) {
-        //number of columns not matched
-        return -2;
+//zoek datum van creeren bestand and return als Peildatum
+BOOL GETFileCreationDate(HANDLE hFile, std::tm& Peildatum) {
+
+    int year, month, day;
+    FILETIME ftCreate, ftAccess, ftWrite;
+    SYSTEMTIME stUTC, stLocal;
+    DWORD dwRet;
+
+    // Retrieve the file times for the file.
+    if (!GetFileTime(hFile, &ftCreate, &ftAccess, &ftWrite))
+        return FALSE;
+
+    // Convert the creation time to local time.
+    FileTimeToSystemTime(&ftCreate, &stUTC);
+    SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+
+    //fill tm struct
+    Peildatum.tm_year = stLocal.wYear - 1900;
+    Peildatum.tm_mon = stLocal.wMonth - 1;
+    Peildatum.tm_mday = stLocal.wDay;
+    return TRUE;
+}
+
+//Zoek importbestand type en verwerk bestand naar gelang type. return 1 als volledig ingelezen, -1 als niet of deels ingelezen.
+int parse_import_file(HWND hWndDLG, std::string filepath_s, std::tm Peildatum) {
+
+    int rc = 0;
+    int checktype = 0;
+    int parsed = 0;
+
+    checktype = check_importtype_csv(filepath_s);
+
+    switch (checktype) {
+    case 1:
+        //schade
+        send_log_lbox_info(hWndDLG, "Start inlezen bessy objecten_import bestand.");
+        parsed = parse_bessy_objectenimport(hWndDLG, filepath_s);
+        break;
+    default:
+        send_log_lbox_info(hWndDLG, "Inhoud importbestand niet herkend.");
+        break;
+
     }
 
-    //for lenght of vector
-    for (unsigned int i = 0; i < col_names.size(); i++) {
-        std::string teststr = col_names[i];
-        if (std::find(col_cmp.begin(), col_cmp.end(), col_names[i]) == col_cmp.end()) {
-            //n/a column not available
-            //todo: output which column not available
-            //make progressbar full en make message all done
-            std::stringstream tmp_str;
-            tmp_str << "Onbekende kolom [" << teststr << "] in bestand. Bestand niet ingelezen!";
-            send_log_lbox_error(hWnd, tmp_str.str());
+    if (parsed == 1) {
+        send_log_lbox_info(hWndDLG, "Importbestand volledig ingelezen.");
+        rc = 1;
+    }
+    else {
+        send_log_lbox_info(hWndDLG, "Importbestand niet (volledig) ingelezen.");
+        rc = -1;
+    }
+    return rc;
+}
 
-            UINT maxrange = SendMessage(hwndProgressBar, PBM_GETRANGE, FALSE, NULL);
-            SendMessage(hwndProgressBar, PBM_SETPOS, maxrange, NULL);
-            SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
-            SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L"Klik op ok om door te gaan");
-            SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
-            return -1;
+//checks for type import file : obj_bessy_import[1] , unknown[-1]
+int check_importtype_csv(std::string filepath_s) {
+
+    if (check_columns_csv(colnames_obj_bessy_import, filepath_s) == 1) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+//checks vector of columns in col_cmp are present in file; if all present outputs 1, else -1
+int check_columns_csv(std::vector<std::string> col_cmp, std::string filepath_s) {
+
+    int rc = -1;
+    //number of columns found
+    int nocolumnsfound = 0;
+    std::stringstream log;
+    int n_rows, n_cols;
+    double delta_progress;
+    std::vector<std::string> colnames;
+
+    //fetch column names
+    try {
+        auto info = get_file_info(filepath_s);
+        n_rows = info.n_rows;
+        n_cols = info.n_cols;
+        colnames = info.col_names;
+        delta_progress = 100 / n_rows;
+
+    }
+    catch (std::exception& e) {
+        return -1;
+    }
+    //for lenght of vector
+    for (unsigned int i = 0; i < colnames.size(); i++) {
+        std::string teststr = colnames[i];
+        if (std::find(col_cmp.begin(), col_cmp.end(), colnames[i]) != col_cmp.end()) {
+
+            nocolumnsfound++;
         }
     }
-    return 1;
+
+    //test if all columns found
+    if (nocolumnsfound == col_cmp.size() - 1) {
+        rc = 1;
+    }
+
+    log.str("");
+    log << "[" << nocolumnsfound << "] van [" << (col_cmp.size() - 1) << "] gevonden in importbestand";
+    send_log_info(log.str());
+    return rc;
 }
 
-int parse_bessy_objectenimport(HWND hWnd, std::string filepath) {
+int parse_bessy_objectenimport(HWND hWndDLG, std::string filepath) {
 
     csv::string_view filepath_v(filepath);
     CSVReader reader(filepath_v);
@@ -425,11 +511,13 @@ int parse_bessy_objectenimport(HWND hWnd, std::string filepath) {
     filename_out.replace(filename_out.end() - 4, filename_out.end(), "_gps_geconverteerd.csv");
     std::ofstream file_out(filename_out);
 
-    int rowno = 1; // 1 = header
-    HWND hwndProgressBar = GetDlgItem(hWnd, IDC_PROGRESS1);
+    std::vector<std::string> col_cmp = colnames_obj_bessy_import;
 
-    send_log_lbox_info(hWnd, filepath);
-    send_log_lbox_info(hWnd, "converteren gps-coordinaten in csv-file gestart");
+    int rowno = 1; // 1 = header
+    HWND hwndProgressBar = GetDlgItem(hWndDLG, IDC_PROGRESS1);
+
+    send_log_lbox_info(hWndDLG, filepath);
+    send_log_lbox_info(hWndDLG, "converteren gps-coordinaten in csv-file");
 
     if (file_out.is_open()) {
         //read file per row (task) and write to output , check for correct gps coordinates.
@@ -452,13 +540,13 @@ int parse_bessy_objectenimport(HWND hWnd, std::string filepath) {
                 std::string tempstr = row[col_cmp[i]].get();
                 file_out << "," << row[col_cmp[i]].get();
             }
-            file_out << "," << check_and_convert_gps(row["GPS Latitude"].get(),0);
-            file_out << "," << check_and_convert_gps(row["GPS Longitude"].get(),1);
+            file_out << "," << check_and_convert_gps(hWndDLG, row["GPS Latitude"].get(),0);
+            file_out << "," << check_and_convert_gps(hWndDLG, row["GPS Longitude"].get(),1);
             file_out << "\n";
 
             //update info log
             tmp_str << "Lijn[" << rowno << "] verwerkt.";
-            send_log_info(tmp_str.str());
+            send_log_lbox_info(hWndDLG, tmp_str.str());
             tmp_str.str("");
 
             //step up progressbar
@@ -467,24 +555,18 @@ int parse_bessy_objectenimport(HWND hWnd, std::string filepath) {
         }
     }
     else {
-        send_log_error("Kan output bestand niet maken!");
+        send_log_lbox_error(hWndDLG, "Kan output bestand niet maken!");
         return -1;
     }
 
     if (file_out.is_open()) { file_out.close(); }
 
-    //make progressbar full en make message all done
-    UINT maxrange = SendMessage(hwndProgressBar, PBM_GETRANGE, FALSE, NULL);
-    SendMessage(hwndProgressBar, PBM_SETPOS, maxrange, NULL);
-    SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
-    SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L"Klik op ok om door te gaan");
-    SendDlgItemMessage(hWnd, IDC_LIST1, LB_INSERTSTRING, 0, (LPARAM)L" ");
-
     return 1;
 }
 
-std::string check_and_convert_gps(std::string input, int latlon) {
+std::string check_and_convert_gps(HWND hWndDLG, std::string input, int latlon) {
     size_t start_pos = 0;
+    int converted = 0;
     std::string ofnlat = "51.86453480159791";
     std::string ofnlon = "5.318332207240367";
     std::string str = input;
@@ -494,7 +576,8 @@ std::string check_and_convert_gps(std::string input, int latlon) {
     while ((start_pos = str.find(",", start_pos)) != std::string::npos) {
         str.replace(start_pos, 1, ".");
         start_pos = start_pos + 2;
-        info << "komma gevonden en vervangen in input [" << input << "]";
+        info << "komma gevonden en verwijderd. Input [" << input << "]";
+        converted = 1;
         send_log_info(info.str());
         info.str("");
     }
@@ -511,6 +594,10 @@ std::string check_and_convert_gps(std::string input, int latlon) {
             else {
                 pointcount++;
                 str.erase(i, 1);
+                info << "dubbele punt gevonden en verwijderd. Input [" << input << "]";
+                send_log_info(info.str());
+                info.str("");
+                converted = 1;
                 i--; // string is opgeschoven, volgende character is nu huidige character.
             }
 
@@ -520,9 +607,17 @@ std::string check_and_convert_gps(std::string input, int latlon) {
     //if no points where found then add point on expected location 
     if (input != "" && pointloc == 0) {
         if (latlon == 0) {
+            info << "geen punt gevonden , toegevoegd. Input [" << input << "]";
+            send_log_info(info.str());
+            info.str("");
+            converted = 1;
             str.insert(2,".");
         }
         if (latlon == 1) {
+            info << "geen punt gevonden , toegevoegd. Input [" << input << "]";
+            send_log_info(info.str());
+            info.str("");
+            converted = 1;
             str.insert(1, ".");
         }
     }
@@ -537,19 +632,37 @@ std::string check_and_convert_gps(std::string input, int latlon) {
     }
     //check number if lattitude
     if (latlon == 0) {
-        if (49 < beforedot_int && beforedot_int > 53) { beforedot_int = 0; }
+        if (49 < beforedot_int && beforedot_int > 53) { 
+            info << "geen geldige GPS [NL] , gps lattitude verwijderd. Input [" << input << "]";
+            send_log_info(info.str());
+            info.str("");
+            converted = 1;
+            str = ""; }
     //check number if Longitude     
     }
     if (latlon == 1) {
-        if (4 < beforedot_int && beforedot_int > 6) { beforedot_int = 0; }
+        if (4 < beforedot_int && beforedot_int > 6) { 
+            info << "geen geldige GPS [NL] , gps longitude verwijderd. Input [" << input << "]";
+            send_log_info(info.str());
+            info.str(""); 
+            converted = 1;
+            str = ""; }
     }
 
     //if string is empty ( input empty or gps corrupted) add OFN coordinates.
     if (str.compare("") == 0) {
         if (latlon == 0) {
+            info << "geen geldige GPS, lattitude aangepast naar OFN [" << ofnlat << "]. Input [" << input << "]";
+            send_log_lbox_info(hWndDLG, info.str());
+            info.str("");
+            converted = 1;
             return ofnlat;
          }
         else if (latlon == 1) {
+            info << "geen geldige GPS, longitude aangepast naar OFN [" << ofnlon << "]. Input [" << input << "]";
+            send_log_lbox_info(hWndDLG, info.str());
+            info.str("");
+            converted = 1;
             return ofnlon;
         }
         else {
@@ -557,6 +670,19 @@ std::string check_and_convert_gps(std::string input, int latlon) {
         }
     }
 
-    //copy new string to output
+    //report listbox and log.
+    if (converted == 1) {
+        if (latlon == 0) {
+            info << "GPS aangepast, lattitude aangepast naar [" << str << "]. Input [" << input << "]";
+            send_log_lbox_info(hWndDLG, info.str());
+            info.str("");
+        }
+        else if (latlon == 1) {
+            info << "GPS aangepast, longitude aangepast naar [" << str << "]. Input [" << input << "]";
+            send_log_lbox_info(hWndDLG, info.str());
+            info.str("");
+        }
+    }
+    
     return str;
  }
--- csv-gps-conversie-tool.h
+++ csv-gps-conversie-tool.h
@@ -5,11 +5,12 @@
 #include <vector>
 
 void opencsvfile(HWND hWnd);
+void read_import_file(HWND hWnd, PWSTR pszFilePath);
+BOOL GETFileCreationDate(HANDLE hFile, std::tm& FilecreationDate);
 
-void read_bessy_object_importfile(HWND hWnd, std::vector<std::string> colnames, std::string filepath_s);
+int parse_import_file(HWND hWndDLG, std::string filepath_s, std::tm Peildatum);
+int check_importtype_csv(std::string filepath_s);
+int check_columns_csv(std::vector<std::string> col_cmp, std::string filepath_s);
 
-int check_bessy_objectenimport(HWND hWnd, std::vector<std::string> col_names);
-
-int parse_bessy_objectenimport(HWND hWnd, std::string filepath_s);
-
-std::string check_and_convert_gps(std::string input,int latlon);
+int parse_bessy_objectenimport(HWND hWndDLG, std::string filepath_s);
+std::string check_and_convert_gps(HWND hWndDLG, std::string input,int latlon);
